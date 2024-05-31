f1b178ff51357a51003b12b05d0bce11170c2fab
duaaxkhan <duaashahidkhan01@gmail.com>
2024-05-31 20:38:55 +0500

    some chnages

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -22,67 +22,14 @@ const Navbar = () => {
                 <div className='hidden md:flex'>
                <ul className='hidden md:flex'>
                 <li>
-                <Link 
-      
-      to="home" 
-      
-      smooth={true} 
-      
-      duration={500} 
-     
-    >
-      Home
-    </Link>
+                <Link to="home"  smooth={true} duration={500} > Home</Link>
                 </li>
-                <li><Link 
-      
-      to="about" 
-      
-      smooth={true} 
-      
-      duration={500} 
-     
-    >
-      About
-    </Link></li>
+                <li><Link to="about" smooth={true} duration={500}>About</Link></li>
                 <li>
-                <Link 
-      
-      to="Skills" 
-      
-      smooth={true} 
-      
-      duration={500} 
-     
-    >
-      Skill
-    </Link>
-                </li>
+                <Link  to="Skills"  smooth={true}  duration={500} >Skills</Link></li>
                 <li>
-                <Link 
-      
-      to="work" 
-      
-      smooth={true} 
-      
-      duration={500} 
-     
-    >
-      Work
-    </Link>
-                </li>
-                <li>
-                <Link 
-      
-      to="contact" 
-      
-      smooth={true} 
-      
-      duration={500} 
-     
-    >
-      Contact
-    </Link>
+                <Link to="work" smooth={true} duration={500} >Work</Link> </li>
+                <li><Link to="contact"  smooth={true} duration={500} > Contact</Link>
                 </li>
                </ul>
             </div>
@@ -96,11 +43,17 @@ const Navbar = () => {
             {/* Mobile Menu */}
             <div className={!nav?'hidden':' absolute top-0 left-0 w-full h-screen bg-[#191616] flex flex-col justify-center items-center'}>
             <ul>
-                <li className='py-6 text-3xl'>About</li>
-                <li className='py-6 text-3xl'>Home</li>
-                <li className='py-6 text-3xl'>Skill</li>
-                <li className='py-6 text-3xl'>Work</li>
-                <li className='py-6 text-3xl'>Contact</li>
+                <li className='py-6 text-3xl'>
+                <Link onClick={handleClick} to="about" smooth={true} duration={500}>About</Link>
+                </li>
+                <li className='py-6 text-3xl'>
+                <Link onClick={handleClick}  to="home"  smooth={true} duration={500} > Home</Link>
+                </li>
+                <li className='py-6 text-3xl'>
+                <Link  onClick={handleClick}  to="Skills"  smooth={true}  duration={500} >Skills</Link>
+                </li>
+                <li className='py-6 text-3xl'><Link  onClick={handleClick} to="work" smooth={true} duration={500} >Work</Link></li>
+                <li className='py-6 text-3xl'><Link onClick={handleClick}  to="contact"  smooth={true} duration={500} > Contact</Link></li>
                </ul>
             </div>
 
@@ -110,7 +63,7 @@ const Navbar = () => {
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://www.linkedin.com/in/duaa-shahid-889323232/'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -118,23 +71,23 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://github.com/duaaxkhan'
             >
               Github <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+          {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='duaashahidkhan01@gmail.com'
             >
               Email <HiOutlineMail size={30} />
             </a>
-          </li>
+          </li> */}
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://drive.google.com/file/d/1VWBejeAu37KYai4U42zzh2_-VGBIXtlo/view'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
--- src/Componenets/Work.jsx
+++ src/Componenets/Work.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import img1 from './../Assests/workImg.jpeg';
-import img2 from './../Assests/realestate.jpg';
+import img2 from './../Assests/gait.guru.jpeg';
 const Work = () => {
+
+  const handleDemoClick = (event) => {
+    event.preventDefault(); // Prevent the default anchor behavior
+    // Replace the URL with your actual demo video URL
+    window.open('https://drive.google.com/file/d/1KD7PntUBj4YsJY_c0wQBrTt-zdbcZurg/view?usp=sharing', '_blank');
+  };
+
+  const handleCodeClick = (event) => {
+    event.preventDefault(); // Prevent the default anchor behavior
+    // Replace the URL with your actual GitHub repository URL
+    window.open('https://github.com/duaaxkhan/gaitguruplus', '_blank');
+  };
   return (
-    <div  name="work" className='w-full md:h-screen bg-[#191616]'>
+    <div  name="work" className=' w-full md:h-screen bg-[#191616]'>
       <div className='max-w-[1000px] mx-auto flex flex-col justify-center items-center w-full h-full text-[#8892b0]'> 
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-[#fcc0c5] text-[#8892b0] sm:text-right '>Work</p>
@@ -11,25 +23,25 @@ const Work = () => {
         </div>
         <div 
         className='grid sm:grid-cols-2 md:grid-cols-3 gap-10'>
-          <div   style={{backgroundImage:`url(${img1})`}}
+          <div   style={{backgroundImage:`url(${img2})`}}
           className='shadow-lg shadow-[#a1696e] group container rounded-md flex justify-center items-center mx-auto content-div'>
           
           
          {/* hover */}
           
            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React Js Application
+              <span className='text-2xl font-bold mb-30 text-white tracking-wider'>
+                Gait.Guru
                 
               </span>
               <div className='pt-8  text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-3 py-3 m-2 bg-[#a1696e] text-white font-bold text-lg'>Demo
+                <a href=''>
+                  <button  onClick={handleDemoClick} className='text-center rounded-lg px-3 py-3 m-2 bg-[#a1696e] text-white font-bold text-lg'>Demo
           
                   </button>
                 </a>
                 <a href='/'>
-                  <button className='text-center rounded-lg px-3 py-3 m-2 bg-[#a1696e] text-white font-bold text-lg'>Code
+                  <button  onClick={handleCodeClick} className='text-center rounded-lg px-3 py-3 m-2 bg-[#a1696e] text-white font-bold text-lg'>Code
                     
                   </button>
                 </a>
