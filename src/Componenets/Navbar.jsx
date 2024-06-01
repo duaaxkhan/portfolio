4c8010c1ab5c1fbd443c71d76e6ea71dcc6fcd95
duaaxkhan <duaashahidkhan01@gmail.com>
2024-06-01 13:46:30 +0500

    .

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1VWBejeAu37KYai4U42zzh2_-VGBIXtlo/view'
+              href='https://drive.google.com/file/d/1ggx_x2I3ay4t_E4CWfUCRpk3QUfFnhfz/view?usp=sharing'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
